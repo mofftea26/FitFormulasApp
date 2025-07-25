d3a8266b0a20e8f6c25d5ddf00db1e553da0b61c
GeorgeMoff <georgemaalouf73@gmail.com>
2025-07-25 15:19:46 +0300

    fix

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,24 +3,24 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const tintColorLight = "#0a7ea4";
+const tintColorDark = "#fff";
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
+    text: "#1F2937",
+    background: "#F9FAFB",
+    tint: "#F43F5E", // rose red
+    icon: "#6B7280",
+    tabIconDefault: "#6B7280",
+    tabIconSelected: "#F43F5E",
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
+    text: "#F3F4F6",
+    background: "#111827",
+    tint: "#F43F5E",
+    icon: "#9CA3AF",
+    tabIconDefault: "#9CA3AF",
+    tabIconSelected: "#F43F5E",
   },
 };
